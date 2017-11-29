01c54afa9bc6887b63522f1e655eba6f6f5a3a96
Sergey Churilin <mavric-2006@yandex.ru>
2017-11-30 01:04:17 +0300

    fix

--- Articles/scripts/main.js
+++ Articles/scripts/main.js
@@ -1,25 +1,9 @@
-let requesterInstance = null;
 let application = null;
-let errorHandlerInstance = null;
 
-document.onkeyup = function (e) {
-    e = e || window.event;
-    if (e.keyCode === 13) {
-        application.onButtonGetArticlePress();
-    }
-    return false;
-};
-
-window.onload = function(){
+window.onload = function () {
     init();
 };
 
-function init(){
+function init() {
     application = new Application();
-    application.createLayout();
 }
-
-
-
-
-
